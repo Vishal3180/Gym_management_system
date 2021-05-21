8df6dc37852c9d77d3f8da5bdbe20ce1a41b2f3f
Vishal Patil <vishalpatil1497@gmail.com>
2021-05-21 19:51:39 +0530

    Add Equipment in gym

---
+++ Addequpment.Designer.vb
@@ -0,0 +1,302 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Addequpment
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Txtperunitcost = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Txtid = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Txttotolcost = New System.Windows.Forms.TextBox()
+        Me.Txtquantity = New System.Windows.Forms.TextBox()
+        Me.Txtname = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.QuantityDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PerUnitCostDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TotalCostDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.EquipmentBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.VishalDataSet5 = New vishal.vishalDataSet5()
+        Me.EquipmentTableAdapter = New vishal.vishalDataSet5TableAdapters.equipmentTableAdapter()
+        Me.Panel1.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.EquipmentBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VishalDataSet5, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Txtperunitcost)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Txtid)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Txttotolcost)
+        Me.Panel1.Controls.Add(Me.Txtquantity)
+        Me.Panel1.Controls.Add(Me.Txtname)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Location = New System.Drawing.Point(12, 80)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(425, 378)
+        Me.Panel1.TabIndex = 17
+        '
+        'Txtperunitcost
+        '
+        Me.Txtperunitcost.Location = New System.Drawing.Point(173, 142)
+        Me.Txtperunitcost.Name = "Txtperunitcost"
+        Me.Txtperunitcost.Size = New System.Drawing.Size(209, 20)
+        Me.Txtperunitcost.TabIndex = 32
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(28, 141)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 18)
+        Me.Label5.TabIndex = 31
+        Me.Label5.Text = "Per unit cost"
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(307, 334)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 30
+        Me.Button3.Text = "Delete"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(173, 334)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 29
+        Me.Button2.Text = "Update"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Txtid
+        '
+        Me.Txtid.Location = New System.Drawing.Point(173, 26)
+        Me.Txtid.Name = "Txtid"
+        Me.Txtid.Size = New System.Drawing.Size(60, 20)
+        Me.Txtid.TabIndex = 28
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(28, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(19, 18)
+        Me.Label2.TabIndex = 27
+        Me.Label2.Text = "Id"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(31, 334)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 26
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Txttotolcost
+        '
+        Me.Txttotolcost.Location = New System.Drawing.Point(173, 273)
+        Me.Txttotolcost.Name = "Txttotolcost"
+        Me.Txttotolcost.Size = New System.Drawing.Size(209, 20)
+        Me.Txttotolcost.TabIndex = 25
+        '
+        'Txtquantity
+        '
+        Me.Txtquantity.Location = New System.Drawing.Point(173, 205)
+        Me.Txtquantity.Name = "Txtquantity"
+        Me.Txtquantity.Size = New System.Drawing.Size(209, 20)
+        Me.Txtquantity.TabIndex = 23
+        '
+        'Txtname
+        '
+        Me.Txtname.Location = New System.Drawing.Point(173, 77)
+        Me.Txtname.Name = "Txtname"
+        Me.Txtname.Size = New System.Drawing.Size(209, 20)
+        Me.Txtname.TabIndex = 22
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(28, 275)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(77, 18)
+        Me.Label6.TabIndex = 21
+        Me.Label6.Text = "Total Cost"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(28, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 18)
+        Me.Label4.TabIndex = 19
+        Me.Label4.Text = "Quantity"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(28, 79)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 18)
+        Me.Label3.TabIndex = 18
+        Me.Label3.Text = "Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(148, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(165, 24)
+        Me.Label1.TabIndex = 18
+        Me.Label1.Text = "Add Equipments"
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.QuantityDataGridViewTextBoxColumn, Me.PerUnitCostDataGridViewTextBoxColumn, Me.TotalCostDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.EquipmentBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(452, 80)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(543, 378)
+        Me.DataGridView1.TabIndex = 19
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'QuantityDataGridViewTextBoxColumn
+        '
+        Me.QuantityDataGridViewTextBoxColumn.DataPropertyName = "Quantity"
+        Me.QuantityDataGridViewTextBoxColumn.HeaderText = "Quantity"
+        Me.QuantityDataGridViewTextBoxColumn.Name = "QuantityDataGridViewTextBoxColumn"
+        '
+        'PerUnitCostDataGridViewTextBoxColumn
+        '
+        Me.PerUnitCostDataGridViewTextBoxColumn.DataPropertyName = "Per_Unit_Cost"
+        Me.PerUnitCostDataGridViewTextBoxColumn.HeaderText = "Per_Unit_Cost"
+        Me.PerUnitCostDataGridViewTextBoxColumn.Name = "PerUnitCostDataGridViewTextBoxColumn"
+        '
+        'TotalCostDataGridViewTextBoxColumn
+        '
+        Me.TotalCostDataGridViewTextBoxColumn.DataPropertyName = "Total_Cost"
+        Me.TotalCostDataGridViewTextBoxColumn.HeaderText = "Total_Cost"
+        Me.TotalCostDataGridViewTextBoxColumn.Name = "TotalCostDataGridViewTextBoxColumn"
+        '
+        'EquipmentBindingSource
+        '
+        Me.EquipmentBindingSource.DataMember = "equipment"
+        Me.EquipmentBindingSource.DataSource = Me.VishalDataSet5
+        '
+        'VishalDataSet5
+        '
+        Me.VishalDataSet5.DataSetName = "vishalDataSet5"
+        Me.VishalDataSet5.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'EquipmentTableAdapter
+        '
+        Me.EquipmentTableAdapter.ClearBeforeFill = True
+        '
+        'Addequpment
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(1013, 480)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Addequpment"
+        Me.Text = "Addequpment"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.EquipmentBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VishalDataSet5, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Txtid As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Txttotolcost As System.Windows.Forms.TextBox
+    Friend WithEvents Txtquantity As System.Windows.Forms.TextBox
+    Friend WithEvents Txtname As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents VishalDataSet5 As vishal.vishalDataSet5
+    Friend WithEvents EquipmentBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents EquipmentTableAdapter As vishal.vishalDataSet5TableAdapters.equipmentTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents QuantityDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PerUnitCostDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TotalCostDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Txtperunitcost As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+End Class
---
+++ Addequpment.vb
@@ -0,0 +1,126 @@
+﻿Imports System.Data.Sql
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class Addequpment
+    Dim sqlcon As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim command As SqlCommand
+    Sub refresh_table()
+        Txtid.Text = ""
+        Txtname.Text = ""
+        Txtperunitcost.Text = ""
+        Txtquantity.Text = ""
+        Txttotolcost.Text = ""
+    End Sub
+    Private Sub Addequpment_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'VishalDataSet5.equipment' table. You can move, or remove it, as needed.
+        Me.EquipmentTableAdapter.Fill(Me.VishalDataSet5.equipment)
+
+    End Sub
+    Sub reload()
+        Txtid.Enabled = False
+
+        Dim con As New SqlConnection("Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True")
+        con.Open()
+        Dim cmd As New SqlCommand("Select * from equipment", con)
+        Dim adapter As New SqlDataAdapter(cmd)
+        Dim table As New DataTable
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+
+    End Sub
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Txtid.Enabled = False
+
+        Txttotolcost.Text = Txtquantity.Text * Txtperunitcost.Text
+
+        Dim sqlcon As New SqlConnection
+        Dim cmd As New SqlCommand
+
+        sqlcon.ConnectionString = "Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True"
+        sqlcon.Open()
+        cmd.Connection = sqlcon
+        cmd.CommandText = "insert into equipment values('" + Txtname.Text + "' ,'" + Txtquantity.Text + "', '" + Txtperunitcost.Text + "','" + Txttotolcost.Text + "')"
+        cmd.Parameters.Add("@Quantity", SqlDbType.Int).Value = Txtquantity.Text
+        cmd.Parameters.Add("@Per_Unit_Cost", SqlDbType.Float).Value = Txtperunitcost.Text
+        cmd.Parameters.Add("@Total_Cost", SqlDbType.Float).Value = Txttotolcost.Text
+
+        cmd.ExecuteNonQuery()
+
+        sqlcon.Close()
+        MessageBox.Show("Data is successfuly..!!!")
+        reload()
+        refresh_table()
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Try
+            sqlcon.ConnectionString = "Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True"
+            sqlcon.Open()
+            Dim query As String
+            Dim READER As SqlDataReader
+            query = "update  equipment set Name='" + Txtname.Text + "',Quantity='" + Txtquantity.Text + "',Per_Unit_Cost='" + Txtperunitcost.Text + "',Total_Cost='" + Txttotolcost.Text + "'where id='" + Txtid.Text + "'"
+            command = New SqlCommand(query, sqlcon)
+            READER = command.ExecuteReader
+            sqlcon.Close()
+            MessageBox.Show("Data is updated successfully...!!")
+            reload()
+            refresh_table()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+
+        Try
+            sqlcon.ConnectionString = "Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True"
+            sqlcon.Open()
+            Dim query As String
+            Dim READER As SqlDataReader
+            query = "Delete from  equipment where id='" + Txtid.Text + "'"
+            command = New SqlCommand(query, sqlcon)
+            READER = command.ExecuteReader
+            sqlcon.Close()
+
+            MessageBox.Show("Data is Deleted successfully..!!")
+            reload()
+            refresh_table()
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+
+        End Try
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(ByVal sender As System.Object, ByVal e As System.Windows.Forms.DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+        Txtid.Enabled = False
+        Try
+            Dim i As Integer = e.RowIndex
+            Dim selectedRow As New DataGridViewRow
+            selectedRow = DataGridView1.Rows(i)
+            Txtid.Text = selectedRow.Cells(0).Value.ToString
+            Txtname.Text = selectedRow.Cells(1).Value.ToString
+            Txtquantity.Text = selectedRow.Cells(2).Value.ToString
+            Txtperunitcost.Text = selectedRow.Cells(3).Value.ToString
+            Txttotolcost.Text = selectedRow.Cells(4).Value.ToString
+
+
+        Catch EX As Exception
+            MessageBox.Show(EX.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Txtquantity_Leave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Txtquantity.Leave
+        Txttotolcost.Text = Txtquantity.Text * Txtperunitcost.Text
+    End Sub
+End Class
