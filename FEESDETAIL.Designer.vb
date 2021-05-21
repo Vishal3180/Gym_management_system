55d51e5aff0ee57b7b3fa341e5d0189bce9bbd31
Vishal Patil <vishalpatil1497@gmail.com>
2021-05-21 19:53:57 +0530

    Fees Detail form in gym for members

---
+++ FEESDETAIL.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FEESDETAIL
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.VishalDataSet7 = New vishal.vishalDataSet7()
+        Me.RegistrationBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.RegistrationTableAdapter = New vishal.vishalDataSet7TableAdapters.RegistrationTableAdapter()
+        Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.MobnoDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TotalFeesDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PayFeesDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PendingfeesDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TimeDurationDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.PayDateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VishalDataSet7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RegistrationBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.NameDataGridViewTextBoxColumn, Me.MobnoDataGridViewTextBoxColumn, Me.TotalFeesDataGridViewTextBoxColumn, Me.PayFeesDataGridViewTextBoxColumn, Me.PendingfeesDataGridViewTextBoxColumn, Me.TimeDurationDataGridViewTextBoxColumn, Me.PayDateDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.RegistrationBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 48)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(843, 304)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'VishalDataSet7
+        '
+        Me.VishalDataSet7.DataSetName = "vishalDataSet7"
+        Me.VishalDataSet7.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'RegistrationBindingSource
+        '
+        Me.RegistrationBindingSource.DataMember = "Registration"
+        Me.RegistrationBindingSource.DataSource = Me.VishalDataSet7
+        '
+        'RegistrationTableAdapter
+        '
+        Me.RegistrationTableAdapter.ClearBeforeFill = True
+        '
+        'IdDataGridViewTextBoxColumn
+        '
+        Me.IdDataGridViewTextBoxColumn.DataPropertyName = "Id"
+        Me.IdDataGridViewTextBoxColumn.HeaderText = "Id"
+        Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
+        Me.IdDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'NameDataGridViewTextBoxColumn
+        '
+        Me.NameDataGridViewTextBoxColumn.DataPropertyName = "Name"
+        Me.NameDataGridViewTextBoxColumn.HeaderText = "Name"
+        Me.NameDataGridViewTextBoxColumn.Name = "NameDataGridViewTextBoxColumn"
+        '
+        'MobnoDataGridViewTextBoxColumn
+        '
+        Me.MobnoDataGridViewTextBoxColumn.DataPropertyName = "Mob_no"
+        Me.MobnoDataGridViewTextBoxColumn.HeaderText = "Mob_no"
+        Me.MobnoDataGridViewTextBoxColumn.Name = "MobnoDataGridViewTextBoxColumn"
+        '
+        'TotalFeesDataGridViewTextBoxColumn
+        '
+        Me.TotalFeesDataGridViewTextBoxColumn.DataPropertyName = "Total_Fees"
+        Me.TotalFeesDataGridViewTextBoxColumn.HeaderText = "Total_Fees"
+        Me.TotalFeesDataGridViewTextBoxColumn.Name = "TotalFeesDataGridViewTextBoxColumn"
+        '
+        'PayFeesDataGridViewTextBoxColumn
+        '
+        Me.PayFeesDataGridViewTextBoxColumn.DataPropertyName = "Pay_Fees"
+        Me.PayFeesDataGridViewTextBoxColumn.HeaderText = "Pay_Fees"
+        Me.PayFeesDataGridViewTextBoxColumn.Name = "PayFeesDataGridViewTextBoxColumn"
+        '
+        'PendingfeesDataGridViewTextBoxColumn
+        '
+        Me.PendingfeesDataGridViewTextBoxColumn.DataPropertyName = "Pending_fees"
+        Me.PendingfeesDataGridViewTextBoxColumn.HeaderText = "Pending_fees"
+        Me.PendingfeesDataGridViewTextBoxColumn.Name = "PendingfeesDataGridViewTextBoxColumn"
+        '
+        'TimeDurationDataGridViewTextBoxColumn
+        '
+        Me.TimeDurationDataGridViewTextBoxColumn.DataPropertyName = "Time_Duration"
+        Me.TimeDurationDataGridViewTextBoxColumn.HeaderText = "Time_Duration"
+        Me.TimeDurationDataGridViewTextBoxColumn.Name = "TimeDurationDataGridViewTextBoxColumn"
+        '
+        'PayDateDataGridViewTextBoxColumn
+        '
+        Me.PayDateDataGridViewTextBoxColumn.DataPropertyName = "Pay_Date"
+        Me.PayDateDataGridViewTextBoxColumn.HeaderText = "Pay_Date"
+        Me.PayDateDataGridViewTextBoxColumn.Name = "PayDateDataGridViewTextBoxColumn"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(298, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(228, 20)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "View Pending_Fees Details"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(379, 358)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 30)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Back"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'FEESDETAIL
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(869, 400)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "FEESDETAIL"
+        Me.Text = "FEESDETAIL"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VishalDataSet7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RegistrationBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents VishalDataSet7 As vishal.vishalDataSet7
+    Friend WithEvents RegistrationBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents RegistrationTableAdapter As vishal.vishalDataSet7TableAdapters.RegistrationTableAdapter
+    Friend WithEvents IdDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents NameDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents MobnoDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TotalFeesDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PayFeesDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PendingfeesDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents TimeDurationDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents PayDateDataGridViewTextBoxColumn As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
---
+++ FEESDETAIL.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.Sql
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class FEESDETAIL
+
+    Sub reload()
+        Dim con As New SqlConnection("Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True")
+        con.Open()
+        Dim cmd As New SqlCommand("Select Id,Name,Mob_no,Total_Fees,Pay_Fees,Pending_fees,Time_Duration,Pay_Date from Registration where Pending_fees>0", con)
+        Dim adapter As New SqlDataAdapter(cmd)
+        Dim table As New DataTable
+        adapter.Fill(table)
+        DataGridView1.DataSource = table
+
+    End Sub
+    Private Sub FEESDETAIL_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'VishalDataSet7.Registration' table. You can move, or remove it, as needed.
+        'Me.RegistrationTableAdapter.Fill(Me.VishalDataSet7.Registration)
+        reload()
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+
+    End Sub
+End Class
