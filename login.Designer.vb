6a511426db59b83169c924437af3bc30e676e6f8
Vishal Patil <vishalpatil1497@gmail.com>
2021-05-21 19:44:22 +0530

    The login form

---
+++ login.Designer.vb
@@ -0,0 +1,167 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class login
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Btnlogin = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Txtpassword = New System.Windows.Forms.TextBox()
+        Me.Txtuser = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.Btnlogin)
+        Me.Panel1.Controls.Add(Me.CheckBox1)
+        Me.Panel1.Controls.Add(Me.Txtpassword)
+        Me.Panel1.Controls.Add(Me.Txtuser)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(257, 232)
+        Me.Panel1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.White
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Location = New System.Drawing.Point(10, 180)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(83, 33)
+        Me.Button1.TabIndex = 25
+        Me.Button1.Text = "Cancel"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Btnlogin
+        '
+        Me.Btnlogin.BackColor = System.Drawing.Color.White
+        Me.Btnlogin.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnlogin.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Btnlogin.Location = New System.Drawing.Point(128, 180)
+        Me.Btnlogin.Name = "Btnlogin"
+        Me.Btnlogin.Size = New System.Drawing.Size(83, 33)
+        Me.Btnlogin.TabIndex = 24
+        Me.Btnlogin.Text = "Login"
+        Me.Btnlogin.UseVisualStyleBackColor = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(128, 154)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(123, 20)
+        Me.CheckBox1.TabIndex = 21
+        Me.CheckBox1.Text = "Show Password"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Txtpassword
+        '
+        Me.Txtpassword.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txtpassword.HideSelection = False
+        Me.Txtpassword.Location = New System.Drawing.Point(6, 116)
+        Me.Txtpassword.Multiline = True
+        Me.Txtpassword.Name = "Txtpassword"
+        Me.Txtpassword.PasswordChar = Global.Microsoft.VisualBasic.ChrW(42)
+        Me.Txtpassword.ShortcutsEnabled = False
+        Me.Txtpassword.Size = New System.Drawing.Size(245, 27)
+        Me.Txtpassword.TabIndex = 20
+        '
+        'Txtuser
+        '
+        Me.Txtuser.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txtuser.Location = New System.Drawing.Point(7, 34)
+        Me.Txtuser.Multiline = True
+        Me.Txtuser.Name = "Txtuser"
+        Me.Txtuser.Size = New System.Drawing.Size(245, 27)
+        Me.Txtuser.TabIndex = 19
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(6, 76)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(86, 20)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Password"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(3, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(86, 16)
+        Me.Label1.TabIndex = 16
+        Me.Label1.Text = "User Name"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.White
+        Me.Panel2.BackgroundImage = Global.vishal.My.Resources.Resources.images__2_
+        Me.Panel2.ForeColor = System.Drawing.Color.Black
+        Me.Panel2.Location = New System.Drawing.Point(275, 12)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(206, 232)
+        Me.Panel2.TabIndex = 2
+        '
+        'login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(493, 254)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.MinimizeBox = False
+        Me.Name = "login"
+        Me.Text = "login"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Txtpassword As System.Windows.Forms.TextBox
+    Friend WithEvents Txtuser As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Btnlogin As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+End Class
---
+++ login.vb
@@ -0,0 +1,46 @@
+﻿Imports System.Data.Sql
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class login
+
+    Private Sub CheckBox1_CheckedChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles CheckBox1.CheckedChanged
+        If Txtpassword.UseSystemPasswordChar = True Then
+            Txtpassword.UseSystemPasswordChar = False
+        Else
+            Txtpassword.UseSystemPasswordChar = True
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+
+    End Sub
+    Private Sub Btnlogin_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btnlogin.Click
+        Dim con As SqlConnection = New SqlConnection("Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True")
+        Dim cmd As SqlCommand = New SqlCommand("select * from login where username='" + Txtuser.Text + "' and password='" + Txtpassword.Text + "'", con)
+        Dim sda As SqlDataAdapter = New SqlDataAdapter(cmd)
+        Dim dt As DataTable = New DataTable()
+        sda.Fill(dt)
+        If (dt.Rows.Count > 0) Then
+            MessageBox.Show(" You are Logined as " + dt.Rows(0)(1))
+
+            mdi.ShowDialog()
+
+        Else
+            MessageBox.Show("User name & passwrod incorrect ")
+        End If
+
+        Txtuser.Text = ""
+        Txtpassword.Text = ""
+    End Sub
+
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
