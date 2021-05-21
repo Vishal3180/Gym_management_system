c7b6f370191297a0f75fa8291ec9a656d1d4eb43
Vishal Patil <vishalpatil1497@gmail.com>
2021-05-21 19:56:47 +0530

    Maintenance form in gym . All equipment and other maintenance

---
+++ Maintance.Designer.vb
@@ -0,0 +1,220 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Maintance
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.txtdate = New System.Windows.Forms.DateTimePicker()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Cmboname = New System.Windows.Forms.ComboBox()
+        Me.Txtamount = New System.Windows.Forms.TextBox()
+        Me.Txtdamage = New System.Windows.Forms.TextBox()
+        Me.Txtid = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(176, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(167, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Maintance Form"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.txtdate)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Cmboname)
+        Me.Panel1.Controls.Add(Me.Txtamount)
+        Me.Panel1.Controls.Add(Me.Txtdamage)
+        Me.Panel1.Controls.Add(Me.Txtid)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 43)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(503, 300)
+        Me.Panel1.TabIndex = 12
+        '
+        'txtdate
+        '
+        Me.txtdate.Location = New System.Drawing.Point(237, 199)
+        Me.txtdate.Name = "txtdate"
+        Me.txtdate.Size = New System.Drawing.Size(200, 20)
+        Me.txtdate.TabIndex = 19
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(30, 199)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(43, 18)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "Date"
+        '
+        'Cmboname
+        '
+        Me.Cmboname.FormattingEnabled = True
+        Me.Cmboname.Location = New System.Drawing.Point(237, 75)
+        Me.Cmboname.Name = "Cmboname"
+        Me.Cmboname.Size = New System.Drawing.Size(200, 21)
+        Me.Cmboname.TabIndex = 17
+        '
+        'Txtamount
+        '
+        Me.Txtamount.Location = New System.Drawing.Point(237, 257)
+        Me.Txtamount.Name = "Txtamount"
+        Me.Txtamount.Size = New System.Drawing.Size(200, 20)
+        Me.Txtamount.TabIndex = 16
+        '
+        'Txtdamage
+        '
+        Me.Txtdamage.Location = New System.Drawing.Point(237, 143)
+        Me.Txtdamage.Name = "Txtdamage"
+        Me.Txtdamage.Size = New System.Drawing.Size(200, 20)
+        Me.Txtdamage.TabIndex = 15
+        '
+        'Txtid
+        '
+        Me.Txtid.Location = New System.Drawing.Point(237, 20)
+        Me.Txtid.Name = "Txtid"
+        Me.Txtid.Size = New System.Drawing.Size(94, 20)
+        Me.Txtid.TabIndex = 14
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(30, 257)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(65, 18)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Amount"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(30, 143)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(174, 18)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Damage of Equipment"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(30, 76)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(52, 18)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Name"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(30, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(21, 18)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Id"
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Location = New System.Drawing.Point(12, 349)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(503, 53)
+        Me.Panel2.TabIndex = 13
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(282, 9)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 29)
+        Me.Button2.TabIndex = 13
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(103, 9)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 29)
+        Me.Button1.TabIndex = 12
+        Me.Button1.Text = "Save"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Maintance
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(536, 414)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Maintance"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Cmboname As System.Windows.Forms.ComboBox
+    Friend WithEvents Txtamount As System.Windows.Forms.TextBox
+    Friend WithEvents Txtdamage As System.Windows.Forms.TextBox
+    Friend WithEvents Txtid As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents txtdate As System.Windows.Forms.DateTimePicker
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+End Class
---
+++ Maintance.vb
@@ -0,0 +1,65 @@
+﻿Imports System.Data.Sql
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class Maintance
+    Dim sqlcon As New SqlConnection
+    Dim cmd As New SqlCommand
+    Dim command As SqlCommand
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+    Public Sub addequipement()
+        
+    End Sub
+    Private Sub Button1_Click_1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Dim sqlcon As New SqlConnection
+        Dim cmd As New SqlCommand
+
+        sqlcon.ConnectionString = "Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True"
+        sqlcon.Open()
+        cmd.Connection = sqlcon
+        cmd.CommandText = "insert into Maintance(Name,Damages,Amount,Date) values('" + Cmboname.Text + "', '" + Txtdamage.Text + "','" + Txtamount.Text + "','" + txtdate.Value + "')"
+        cmd.Parameters.AddWithValue("@Date", Convert.ToDateTime(txtdate.Value))
+
+        cmd.ExecuteNonQuery()
+
+        sqlcon.Close()
+        MessageBox.Show("Data is successfuly..!!!")
+    End Sub
+
+    Private Sub Cmboname_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cmboname.SelectedIndexChanged
+        addequipement()
+    End Sub
+
+    Private Sub Panel1_Paint(ByVal sender As System.Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+
+    Private Sub Maintance_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Try
+            Dim row_count As Integer
+            Dim con As New SqlConnection("Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True")
+            con.Open()
+            Dim cmd2 As New SqlCommand("select count(Name) from equipment", con)
+            row_count = IIf(IsDBNull(cmd2.ExecuteScalar), "", cmd2.ExecuteScalar)
+            Dim cmd As New SqlCommand("select Name from equipment ", con)
+            Dim adapter As New SqlDataAdapter(cmd)
+            Dim table As New DataTable
+            adapter.Fill(table)
+            Dim i As Integer = 0
+            While i < row_count
+                Cmboname.Items.Add(table.Rows(i)(0).ToString())
+                i = i + 1
+
+            End While
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Me.Close()
+
+    End Sub
+End Class
