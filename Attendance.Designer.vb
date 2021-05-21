5664df8f0809fbf673d7d529b308a0c5efa780e5
Vishal Patil <vishalpatil1497@gmail.com>
2021-05-21 19:58:33 +0530

    Addendance form in members

---
+++ Attendance.Designer.vb
@@ -0,0 +1,138 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Attendance
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.BtnIntime = New System.Windows.Forms.Button()
+        Me.Btnouttime = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Txtid = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(55, 81)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(94, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Fees Total"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(109, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(187, 24)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Maintance Balance"
+        '
+        'BtnIntime
+        '
+        Me.BtnIntime.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnIntime.Location = New System.Drawing.Point(62, 291)
+        Me.BtnIntime.Name = "BtnIntime"
+        Me.BtnIntime.Size = New System.Drawing.Size(75, 33)
+        Me.BtnIntime.TabIndex = 5
+        Me.BtnIntime.Text = "Submit"
+        Me.BtnIntime.UseVisualStyleBackColor = True
+        '
+        'Btnouttime
+        '
+        Me.Btnouttime.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Btnouttime.Location = New System.Drawing.Point(272, 291)
+        Me.Btnouttime.Name = "Btnouttime"
+        Me.Btnouttime.Size = New System.Drawing.Size(87, 33)
+        Me.Btnouttime.TabIndex = 6
+        Me.Btnouttime.Text = "Cancel"
+        Me.Btnouttime.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(55, 167)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(137, 20)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Maintance Total"
+        '
+        'Txtid
+        '
+        Me.Txtid.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Txtid.Location = New System.Drawing.Point(201, 81)
+        Me.Txtid.Name = "Txtid"
+        Me.Txtid.Size = New System.Drawing.Size(141, 22)
+        Me.Txtid.TabIndex = 14
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(201, 165)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(141, 22)
+        Me.TextBox1.TabIndex = 15
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(58, 240)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(137, 20)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Maintance Total"
+        '
+        'Attendance
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(420, 336)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Txtid)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Btnouttime)
+        Me.Controls.Add(Me.BtnIntime)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Name = "Attendance"
+        Me.Text = "Attendance"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents BtnIntime As System.Windows.Forms.Button
+    Friend WithEvents Btnouttime As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Txtid As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+End Class
---
+++ Attendance.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Data.Sql
+Imports System.Data
+Imports System.Data.SqlClient
+Imports System.Windows.Forms
+Public Class Attendance
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+    Sub tb()
+
+        'Btnouttime.Text = DateTime.Now.TimeOfDay.ToString()
+    End Sub
+    Private Sub Attendance_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        'BtnIntime.Text = DateTime.Now.TimeOfDay.ToString()
+        'Btnouttime.Text = DateTime.Now.TimeOfDay.ToString()
+        '    lbldate.Text = DateTime.Now.ToShortDateString()
+        '    Try
+        '        Dim row_count As Integer
+        '        Dim con As New SqlConnection("Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True")
+        '        con.Open()
+        '        Dim cmd2 As New SqlCommand("select count(Name) from Registration", con)
+        '        row_count = IIf(IsDBNull(cmd2.ExecuteScalar), "", cmd2.ExecuteScalar)
+        '        Dim cmd As New SqlCommand("select Name from Registration ", con)
+        '        Dim adapter As New SqlDataAdapter(cmd)
+        '        Dim table As New DataTable
+        '        adapter.Fill(table)
+        '        con.Close()
+        '        Dim i As Integer = 0
+        '        While i < row_count
+        '            Cmboname.Items.Add(table.Rows(i)(0).ToString())
+        '            i = i + 1
+
+        '        End While
+        '    Catch ex As Exception
+
+        '    End Try
+        'End Sub
+
+        'Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnIntime.Click
+        '    'BtnIntime.Text = DateTime.Now.TimeOfDay.ToString()
+        '    'Try
+        '    'Dim time As String = DateTime.Now.TimeOfDay.ToString()
+        '    Dim dat As DateTime
+        '    dat = txtdate.Value
+        '    Dim sts As Integer = 1
+        '    Dim sqlcon As New SqlConnection("Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True")
+        '    sqlcon.Open()
+        '    Dim cmd As New SqlCommand("INSERT INTO Attendance(Name,ToDay_Date,In_Time,Status)VALUES('" + Cmboname.Text + "',Convert(DateTime,@dat,103),Convert(DateTime,@dat,103),'" + Convert.ToInt32(sts) + "')", sqlcon)
+        '    cmd.Parameters.AddWithValue("@dat", dat)
+        '    cmd.Parameters.AddWithValue("@dat", dat)
+        '    cmd.ExecuteNonQuery()
+        '    sqlcon.Close()
+
+        '    ' Catch ex As Exception
+        '    'MessageBox.Show(ex.Message)
+        '    ' End Try
+        'End Sub
+
+        'Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Btnouttime.Click
+        '    Dim sqlcon As New SqlConnection
+        '    Dim cmd As New SqlCommand
+
+        '    sqlcon.ConnectionString = "Data Source=DESKTOP-BDHSF78\SQLEXPRESS;Initial Catalog=vishal;Integrated Security=True"
+        '    sqlcon.Open()
+        '    Dim query As String
+        '    Dim READER As SqlDataReader
+        '    query = "update  Attendance set Name='" + Btnouttime.Text + "'where id='" + txtdate.Text + "'"
+        '    sqlcon.Close()
+
+
+        'End Sub
+
+        'Private Sub Cmboname_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs)
+
+    End Sub
+End Class
